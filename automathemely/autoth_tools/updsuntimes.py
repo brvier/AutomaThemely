bee8e0b968023955c3757cd3b4da75c55d55b4ca
Benoît HERVIER <b@rvier.fr>
2020-03-28 14:15:56 +0100

    Fix for astral 2.0

--- automathemely/autoth_tools/updsuntimes.py
+++ automathemely/autoth_tools/updsuntimes.py
@@ -5,7 +5,7 @@ from time import sleep
 
 import pytz
 import tzlocal
-from astral import Location
+from astral import LocationInfo
 
 from automathemely.autoth_tools.utils import get_local, verify_desktop_session
 
@@ -16,12 +16,16 @@ def get_loc_from_ip():
     import requests
 
     #   Try to reach ipinfo a max of 15 times, if no connection or html response not OK log and wait
-    con_err_msg = 'Can\'t reach https://ipinfo.io/ (Network may be down). Waiting 5 minutes to try again...'
+    con_err_msg = "Can't reach https://ipinfo.io/ (Network may be down). Waiting 5 minutes to try again..."
     for i in range(15):
         try:
-            g = requests.get('https://ipinfo.io/json')
-        except (requests.exceptions.ConnectionError, ConnectionAbortedError,
-                ConnectionRefusedError, ConnectionResetError):
+            g = requests.get("https://ipinfo.io/json")
+        except (
+            requests.exceptions.ConnectionError,
+            ConnectionAbortedError,
+            ConnectionRefusedError,
+            ConnectionResetError,
+        ):
             logger.warning(con_err_msg)
             sleep(300)
         else:
@@ -35,60 +39,73 @@ def get_loc_from_ip():
 
 
 def main(us_se):
-    if 'location' not in us_se:
-        logger.error('Invalid config file')
+    if "location" not in us_se:
+        logger.error("Invalid config file")
         return
 
-    if us_se['location']['auto_enabled']:
+    if us_se["location"]["auto_enabled"]:
         loc = get_loc_from_ip()
         if loc:
             loc = loc.json()
         else:
-            logger.error('Couldn\'t connect to ipinfo, giving up')
+            logger.error("Couldn't connect to ipinfo, giving up")
             return
 
-        loc['latitude'], loc['longitude'] = (float(x) for x in loc['loc'].strip().split(','))
-        loc['time_zone'] = tzlocal.get_localzone().zone
+        loc["latitude"], loc["longitude"] = (
+            float(x) for x in loc["loc"].strip().split(",")
+        )
+        loc["time_zone"] = tzlocal.get_localzone().zone
 
     else:
-        for k, v in us_se['location']['manual'].items():
+        for k, v in us_se["location"]["manual"].items():
             try:
                 val = v.strip()
             except AttributeError:
                 pass
             else:
                 if not val:
-                    logger.error('Auto location is not enabled and some manual values are missing')
+                    logger.error(
+                        "Auto location is not enabled and some manual values are missing"
+                    )
                     return
-        loc = us_se['location']['manual']
+        loc = us_se["location"]["manual"]
 
     try:
-        location = Location()
-        location.name = loc['city']
-        location.region = loc['region']
-        location.latitude = loc['latitude']
-        location.longitude = loc['longitude']
-        location.timezone = loc['time_zone']
+        location = LocationInfo()
+        location.name = loc["city"]
+        location.region = loc["region"]
+        location.latitude = loc["latitude"]
+        location.longitude = loc["longitude"]
+        location.timezone = loc["time_zone"]
     except ValueError as e:
         logger.error(str(e))
         return
 
-    sunrise = location.sun()['sunrise'].replace(second=0) + timedelta(minutes=us_se['offset']['sunrise'])
-    sunset = location.sun()['sunset'].replace(second=0) + timedelta(minutes=us_se['offset']['sunset'])
+    sunrise = location.sun()["sunrise"].replace(second=0) + timedelta(
+        minutes=us_se["offset"]["sunrise"]
+    )
+    sunset = location.sun()["sunset"].replace(second=0) + timedelta(
+        minutes=us_se["offset"]["sunset"]
+    )
 
     #   Convert to UTC for storage
     return sunrise.astimezone(pytz.utc), sunset.astimezone(pytz.utc)
 
 
 #   This should only be called when running through systemd
-if __name__ == '__main__':
+if __name__ == "__main__":
     import json
     import pickle as pkl
     import logging
 
     # When importing automathemely we inherit the root logger, so we need to configure it for our purposes
-    from automathemely import main_file_handler, notifier_handler, updsun_file_handler, \
-        default_simple_format, timed_details_format
+    from automathemely import (
+        main_file_handler,
+        notifier_handler,
+        updsun_file_handler,
+        default_simple_format,
+        timed_details_format,
+    )
 
     # I know there also is logging.root, but I found it has some weird behaviours
     root_logger = logging.getLogger()
@@ -104,20 +121,22 @@ if __name__ == '__main__':
         handler.setFormatter(logging.Formatter(timed_details_format))
 
     # This logger is specifically for displaying a notification if something went wrong
-    run_as_main_logger = logging.getLogger('updsuntimes.py')
+    run_as_main_logger = logging.getLogger("updsuntimes.py")
 
     notifier_handler.setLevel(logging.WARNING)
     notifier_handler.setFormatter(logging.Formatter(default_simple_format))
     run_as_main_logger.addHandler(notifier_handler)
 
-    with open(get_local('user_settings.json'), 'r') as f:
+    with open(get_local("user_settings.json"), "r") as f:
         user_settings = json.load(f)
 
     output = main(user_settings)
     if output:
-        with open(get_local('sun_times'), 'wb') as file:
+        with open(get_local("sun_times"), "wb") as file:
             pkl.dump(output, file, protocol=pkl.HIGHEST_PROTOCOL)
     else:
         if verify_desktop_session():
-            run_as_main_logger.warning('There were some errors while updating the sunrise and sunset times, check {} '
-                                       'for more details'.format(get_local('.updsuntimes.log')))
+            run_as_main_logger.warning(
+                "There were some errors while updating the sunrise and sunset times, check {} "
+                "for more details".format(get_local(".updsuntimes.log"))
+            )
